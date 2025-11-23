163434b4adef9ff18a35fac3450393e93e34d284
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2025-11-23 18:54:02 +0100

    Initial Commit

---
+++ RecipeJoe/RecipeJoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeJoeApp.swift
+//  RecipeJoe
+//
+//  Created by Oliver Staub on 23.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct RecipeJoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecipeJoeTests/RecipeJoeTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeJoeTests.swift
+//  RecipeJoeTests
+//
+//  Created by Oliver Staub on 23.11.2025.
+//
+
+import Testing
+@testable import RecipeJoe
+
+struct RecipeJoeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
